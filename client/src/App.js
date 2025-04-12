238e6513cb9d4cd0ec7dd329d7bd522e3446a899
singerztournament <zachary@hoops555.com>
2025-04-12 13:04:20 -0700

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Tournament System</h1>
+      <p>This is a placeholder frontend. Full features coming next.</p>
+    </div>
+  );
+}
+
+export default App;
